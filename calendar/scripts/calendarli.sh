a9e0eb2025bc12545ba1f5153620944d74799ab4
JaiStArX <jaistarx.in@gmail.com>
2020-04-26 15:38:43 +0530

    updated

--- calendar/scripts/calendarli.sh
+++ calendar/scripts/calendarli.sh
@@ -1,3 +1,4 @@
 cd ..
+./mvnw clean package
 ./mvnw "-Dexec.args=-classpath %classpath calender" -Dexec.executable=java org.codehaus.mojo:exec-maven-plugin:1.5.0:exec
 #./mvnw exec:java -D exec.mainClass=calender
--- calendar/scripts/calendarwi.cmd
+++ calendar/scripts/calendarwi.cmd
@@ -1,3 +1,5 @@
 cd ..
+mvnw.cmd clean package
 mvnw.cmd exec:java -D exec.mainClass=calender
-REM ./mvnw.cmd "-Dexec.args=-classpath %classpath calender" -Dexec.executable=java org.codehaus.mojo:exec-maven-plugin:1.5.0:exec
+REM mvnw.cmd "-Dexec.args=-classpath %classpath calender" -Dexec.executable=java org.codehaus.mojo:exec-maven-plugin:1.5.0:exec
+
