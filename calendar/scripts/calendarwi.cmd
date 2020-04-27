cff452d5e81a8f9f0d83521f00a4876140bb422f
Jaison <57367854+jaistarx@users.noreply.github.com>
2020-04-27 16:36:14 +0530

    Update calendarwi.cmd

--- calendar/scripts/calendarwi.cmd
+++ calendar/scripts/calendarwi.cmd
@@ -1,5 +1,4 @@
-cd ..
-mvnw.cmd clean package
-mvnw.cmd exec:java -D exec.mainClass=calender
+call cd ..
+call mvnw.cmd clean package
+call mvnw.cmd exec:java -D exec.mainClass=calender
 REM mvnw.cmd "-Dexec.args=-classpath %classpath calender" -Dexec.executable=java org.codehaus.mojo:exec-maven-plugin:1.5.0:exec
-
